a443279f79deb95f247d4ce8560a20b215ea687a
aidant <15520814+aidant@users.noreply.github.com>
2021-09-02 21:46:28 +1000

    Add comments

--- src/discord/ensure-audio-player.ts
+++ src/discord/ensure-audio-player.ts
@@ -3,9 +3,17 @@ import { createAudioPlayer, NoSubscriberBehavior, VoiceConnectionStatus } from '
 import { namespace } from '../log.js'
 
 const log = namespace('discord:audio-player')
-
+/*
+  By keeping a weak reference to the audio player from the voice connection, we
+  can ensure the voice connection and the audio player are correctly garbage
+  collected. Otherwise we would be keeping a reference to the voice connection
+  and preventing both objects from being destroyed. 
+*/
 const players = new WeakMap<VoiceConnection, AudioPlayer>()
 
+/*
+  Ensure we have a audio player for a given connection.
+*/
 export const ensureAudioPlayer = (connection: VoiceConnection): AudioPlayer => {
   log('begin creating audio player')
 
@@ -18,6 +26,10 @@ export const ensureAudioPlayer = (connection: VoiceConnection): AudioPlayer => {
 
   log('existing audio player: %s', isNewPlayer ? 'no' : 'yes')
 
+  /*
+    If we have just created the audio player, we need to hook it up to the
+    connection and make sure we destroy it with the connection.
+  */
   if (isNewPlayer) {
     connection.subscribe(player)
 
@@ -25,12 +37,11 @@ export const ensureAudioPlayer = (connection: VoiceConnection): AudioPlayer => {
       log('connection destroyed, stopping audio player')
 
       player.off('error', log)
-      player.stop(true)
       players.delete(connection)
+      player.stop(true)
     })
 
     player.on('error', log)
-    
     players.set(connection, player)
   }
 
--- src/discord/ensure-audio-queue.ts
+++ src/discord/ensure-audio-queue.ts
@@ -1,16 +1,29 @@
-import { AudioPlayer, AudioResource, VoiceConnection, VoiceConnectionStatus } from '@discordjs/voice'
+import {
+  AudioPlayer,
+  AudioResource,
+  VoiceConnection,
+  VoiceConnectionStatus,
+} from '@discordjs/voice'
 import { AudioPlayerStatus, createAudioResource, StreamType } from '@discordjs/voice'
 import type { Readable } from 'stream'
 import { namespace } from '../log.js'
 
 const log = namespace('discord:audio-queue')
-
+/*
+  By keeping a weak reference to the audio queue from the audio player, we can
+  ensure the audio player and the audio queue are correctly garbage collected.
+  Otherwise we would be keeping a reference to the audio player and preventing
+  both objects from being destroyed. 
+*/
 const queues = new WeakMap<AudioPlayer, AudioQueue>()
 
 interface AudioQueue {
   add(stream: Readable): void
 }
 
+/*
+  Ensure we have an audio queue for a given audio player.
+*/
 export const ensureAudioQueue = (connection: VoiceConnection, player: AudioPlayer): AudioQueue => {
   log('begin creating audio queue')
 
@@ -21,15 +34,15 @@ export const ensureAudioQueue = (connection: VoiceConnection, player: AudioPlaye
   if (isNewQueue) {
     const resources: AudioResource[] = []
     let timeout: NodeJS.Timeout | undefined
-    
+
     const queue: AudioQueue = {
       add: (stream: Readable) => {
         log('adding resource to queue')
-    
+
         resources.push(createAudioResource(stream, { inputType: StreamType.Arbitrary }))
-    
+
         log('queue length: %d', resources.length)
-    
+
         if (timeout) {
           log('clearing idle timeout')
 
@@ -38,14 +51,17 @@ export const ensureAudioQueue = (connection: VoiceConnection, player: AudioPlaye
 
         if (player.state.status === AudioPlayerStatus.Idle) {
           log('player is idle, queueing resource now')
-    
+
           player.play(resources.shift()!)
         }
-      } 
+      },
     }
-    
+
     queues.set(player, queue)
 
+    /*
+      Kill the queue once the connection is destroyed.
+    */
     const handleConnectionDestroyed = () => {
       log('connection destroyed, killing queue')
 
@@ -53,20 +69,25 @@ export const ensureAudioQueue = (connection: VoiceConnection, player: AudioPlaye
       queues.delete(player)
     }
 
+    /*
+      Once the current audio resource has finished playing, begin playing the
+      next audio resource in the queue. Otherwise if the queue is empty, set an
+      idle timeout.
+    */
     const handlePlayerIdle = () => {
       log('player is idle')
-  
+
       const resource = resources.shift()
-  
+
       if (resource) {
         log('queueing resource now')
-  
+
         player.play(resource)
 
         log('queue length: %d', resources.length)
       } else {
         log('queue is empty, setting idle timeout')
-  
+
         timeout = setTimeout(() => {
           log('idle timeout triggered, killing queue')
 
